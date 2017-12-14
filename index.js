35f60a5c624a24d093d775f8489c528ff5509b86
Melody <Melody@Melodys-MacBook.local>
2017-12-14 14:59:08 -0700

    step 5

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = reqiure('express');
 const bodyParser = reqiure('body-parser');
 const massive = reqiure('massive');
 const cors = reqiure('cors');
+const controller = require('./products_controller');
 require('dotenv').config()
 
 const app = express();
@@ -11,6 +12,12 @@ app.use(cors());
 massive(process.env.CONNECTION_STRING).then(db => {
     app.set('db',db)
 }).catch()
+
+app.post('/api/product', controller.create)
+app.get('/api/products', controller.getAll)
+app.get('/api/product/:id', controller.getOne)
+app.put('/api/product/:id', controller.update)
+app.delete('/api/product/:id', controller.delete)
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`listening on port ${port} <(^_^)^`));
