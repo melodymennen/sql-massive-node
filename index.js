6a02446927ac860ac8d0a86bad65bde1a9488642
Melody <Melody@Melodys-MacBook.local>
2017-12-14 14:32:07 -0700

    step 21

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db',db)
+}).catch()
+
 const port = process.env.PORT
 app.listen(port, () => console.log(`listening on port ${port} <(^_^)^`));
